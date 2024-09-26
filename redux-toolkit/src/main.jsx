23bd333dc082387e3df5591c2b87031511697cc6
Anilch12 <anilch5199@gmail.com>
2024-09-26 18:57:18 +0530

    [Hot Fix]

--- redux-toolkit/src/main.jsx
+++ redux-toolkit/src/main.jsx
@@ -2,8 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {Provider} from'react-redux'
-import Store from './Store.js'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Store from './Component.js/Store.js'
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
       <App />
